88e8a8abbd4538445d862ce99a4b0632d29502f2
Rana <eng.ranafouad.2013@gmail.com>
2016-06-12 12:08:26 +0200

    add map

--- IndoorMapping/www/js/app.js
+++ IndoorMapping/www/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('IndoorMapping', ['ionic','Devise'])
+var app = angular.module('IndoorMapping', ['ionic','Devise','ngCordovaBeacon'])
 
  app.config([
   '$stateProvider',
@@ -41,6 +41,15 @@ var app = angular.module('IndoorMapping', ['ionic','Devise'])
                   $state.go('/home');
                 })
               }]
+  })
+
+  // map
+
+  .state('map',{
+    url: '/map',
+    templateUrl: 'templates/map.html',
+    controller: 'mapCtrl',
+    controllerAs: "mapCtrl",
   });
 
 
@@ -73,6 +82,50 @@ function($state, Auth){
   };
 
 }]);
+
+// map controller
+
+app.controller("mapCtrl",['$rootScope', '$ionicPlatform', '$cordovaBeacon', function($rootScope, $ionicPlatform, $cordovaBeacon){
+    var self = this;
+    self.beacons =  [
+                      {
+                        id: 'zone1',
+                        uuid: '73676723-7400-0000-FFFF-0000FFFF0005',
+                        major: 2,
+                        minor: 746
+                      },
+                      {
+                        id: 'zone2',
+                        uuid: '73676723-7400-0000-FFFF-0000FFFF0001',
+                        major: 2,
+                        minor: 277
+                      }
+                      ];
+    self.startScanForBeacons = function(){
+        var locationManager =  cordova.plugins.locationManager;
+        for (var i = 0; i < self.beacons.length; i++) {
+        var beaconRegion = new locationManager.BeaconRegion(self.beacons[i].id,self.beacons[i].uuid, self.beacons[i].major, self.beacons[i].minor);
+        locationManager.startMonitoringForRegion(beaconRegion).fail(console.error).done();
+    }
+  $ionicPlatform.ready(function(){
+    self.startScanForBeacons();
+  });
+    var delegate = new cordova.plugins.locationManager.Delegate();
+    locationManager.setDelegate(delegate);
+    console.log('self.delegate'+JSON.stringify(delegate));
+    // Detect beacon callback
+    delegate.didDetermineStateForRegion = function (pluginResult) {
+      var beacon = pluginResult.region;
+      console.log('self.beacon'+JSON.stringify(beacon));
+      var state = pluginResult.state === 'CLRegionStateInside' ? true : false;
+      var stateStr = state ? 'in' : 'out';
+      console.log('self.stateStr'+stateStr);
+      //Write code to do whatever you want
+    };
+  };
+}]);
+
+
 
 
 app.run(function($ionicPlatform) {
