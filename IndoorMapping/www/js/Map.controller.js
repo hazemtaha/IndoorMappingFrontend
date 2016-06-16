3e358c9478dd2562ffa65a189fd36d6529611c11
Hazem Taha <thatskan@gmail.com>
2016-06-16 17:26:03 +0200

    enhance map navigation with accuracy and save visits in db

--- IndoorMapping/www/js/Map.controller.js
+++ IndoorMapping/www/js/Map.controller.js
@@ -5,10 +5,10 @@
     .module('IndoorMapping')
     .controller('MapCtrl', mapCtrl);
 
-  mapCtrl.$inject = ['$rootScope', '$ionicPlatform', '$cordovaBeacon', 'Db'];
+  mapCtrl.$inject = ['$rootScope', '$ionicPlatform', '$cordovaBeacon', 'Db', '$timeout'];
 
   /* @ngInject */
-  function mapCtrl($rootScope, $ionicPlatform, $cordovaBeacon, Db) {
+  function mapCtrl($rootScope, $ionicPlatform, $cordovaBeacon, Db, $timeout) {
     var self = this;
     self.svg = SVG('drawing').size('1000', '1000');
     // self.beacons = [{
@@ -22,72 +22,12 @@
     //   major: 2,
     //   minor: 277
     // }];
-    // console.log(JSON.stringify(self.beacons));
-    self.intersection = function(x0, y0, r0, x1, y1, r1) {
-      var a, dx, dy, d, h, rx, ry;
-      var x2, y2;
-
-      /* dx and dy are the vertical and horizontal distances between
-       * the circle centers.
-       */
-      dx = x1 - x0;
-      dy = y1 - y0;
-
-      /* Determine the straight-line distance between the centers. */
-      d = Math.sqrt((dy * dy) + (dx * dx));
-
-      /* Check for solvability. */
-      if (d > (r0 + r1)) {
-        /* no solution. circles do not intersect. */
-        return false;
-      }
-      if (d < Math.abs(r0 - r1)) {
-        /* no solution. one circle is contained in the other */
-        return false;
-      }
-
-      /* 'point 2' is the point where the line through the circle
-       * intersection points crosses the line between the circle
-       * centers.
-       */
-
-      /* Determine the distance from point 0 to point 2. */
-      a = ((r0 * r0) - (r1 * r1) + (d * d)) / (2.0 * d);
-
-      /* Determine the coordinates of point 2. */
-      x2 = x0 + (dx * a / d);
-      y2 = y0 + (dy * a / d);
-
-      /* Determine the distance from point 2 to either of the
-       * intersection points.
-       */
-      h = Math.sqrt((r0 * r0) - (a * a));
-
-      /* Now determine the offsets of the intersection points from
-       * point 2.
-       */
-      rx = -dy * (h / d);
-      ry = dx * (h / d);
-
-      /* Determine the absolute intersection points. */
-      var xi = x2 + rx;
-      var xi_prime = x2 - rx;
-      var yi = y2 + ry;
-      var yi_prime = y2 - ry;
-
-      return {
-        x1: xi,
-        x2: xi_prime,
-        y1: yi,
-        y2: yi_prime
-      };
-    }
     self.startScanForBeacons = function() {
       var locationManager = cordova.plugins.locationManager;
-      // console.log(JSON.stringify(self.beacons));
       for (var i = 0; i < self.beacons.length; i++) {
+        console.log(JSON.stringify(self.beacons));
         var beaconRegion = new locationManager.BeaconRegion(self.beacons[i].id, self.beacons[i].uuid, self.beacons[i].major, self.beacons[i].minor);
-        locationManager.startMonitoringForRegion(beaconRegion).fail(console.error).done();
+        locationManager.startMonitoringForRegion(beaconRegion).fail().done();
         locationManager.startRangingBeaconsInRegion(beaconRegion).fail().done();
       }
       var delegate = new cordova.plugins.locationManager.Delegate();
@@ -99,46 +39,61 @@
         var stateStr = state ? 'in' : 'out';
         //Write code to do whatever you want
       };
-      var rssiObj = {};
+      self.beaconsInRange = {};
+      var isVisitedB4 = false;
       delegate.didRangeBeaconsInRegion = function(data) {
-        // console.log('didRangeBeaconsInRegion: ' + JSON.stringify(data.region.identifier + ' : Prox : ' + data.beacons[0].proximity + ' - rssi: ' + data.beacons[0].rssi + ' - tx: ' + data.beacons[0].tx + ' - accuracy: ' + data.beacons[0].accuracy));
-        // console.log("proximity"+JSON.stringify(data.beacons[0].proximity));
-        //console.log(JSON.stringify(data));
-        if(data.beacons[0].proximity == "ProximityNear")
-         {
-           console.log("data  "+JSON.stringify(data.region.uuid));
-           var beaconUuid = JSON.stringify(data.region.uuid);
-           Db.importMap(data.region.uuid).then(function(response) {
-             console.log("Hello1");
+        var proximity = data.beacons[0].proximity;
+        var accuracy = data.beacons[0].accuracy;
+        if (proximity == "ProximityFar") {
+          delete self.beaconsInRange[data.region.uuid];
+        }
+        var targetBeacon;
+        if (proximity == "ProximityNear" || proximity == "ProximityImmediate") {
+          Db.importMap(data.region.uuid).then(function(response) {
+            self.beacons.forEach(function(beacon) {
+              if (beacon.uuid.toUpperCase() == data.region.uuid.toUpperCase()) {
+                targetBeacon = beacon;
+                var proxValue;
+                if (proximity == "ProximityNear") {
+                  proxValue = 1;
+                } else {
+                  proxValue = 2;
+                }
+                targetBeacon.proxValue = proxValue;
+                targetBeacon.accuracy = accuracy;
+                self.beaconsInRange[targetBeacon.uuid] = targetBeacon;
+              }
+            });
+            console.log(JSON.stringify(targetBeacon.id) + ' - ' + data.beacons[0].accuracy + ' ' + data.beacons[0].proximity);
 
-             //console.log(JSON.stringify(response));
-             console.log(JSON.stringify(self.beacons));
-             var targetBeacon;
-             self.beacons.forEach(function(beacon){
-               if(beacon.uuid.toUpperCase() == data.region.uuid.toUpperCase())
-               {
-                 targetBeacon = beacon;
-               }
-             });
-            //  console.log("targetBeacon"+JSON.stringify(targetBeacon.x));
-             self.svg.svg(response.data.svg_code);
-             self.svg.circle(20).attr('fill', 'black').move(targetBeacon.x,targetBeacon.y);
+            self.svg.svg(response.data.svg_code);
+            // console.log(JSON.stringify(self.beaconsInRange));
+            angular.forEach(self.beaconsInRange, function(beacon) {
+              if (targetBeacon.proxValue < beacon.proxValue) {
+                targetBeacon = beacon;
+              } else if (targetBeacon.accuracy > beacon.accuracy) {
+                targetBeacon = beacon;
+              }
+            });
+            if (!targetBeacon.isVisitedB4) {
+              var userId = window.localStorage.getItem("userId");
+              Db.registerVisit(userId, targetBeacon.uuid);
+              targetBeacon.isVisitedB4 = true;
+            }
+            console.log(targetBeacon.isVisitedB4);
+            $timeout(function() {
+              angular.forEach(self.beaconsInRange, function(beacon) {
+                beacon.isVisitedB4 = false;
+              });
+            }, ((60 * 1000) * 30));
+            self.svg.circle(20).attr('fill', '#98bdc5').move(targetBeacon.x, targetBeacon.y);
 
-           },function(response) {
-             //console.log(JSON.stringify(response));
-              console.log("Hello2");
-           });
-         }
-         else {
-           console.log("hello3");
-         }
-          console.log('didRangeBeaconsInRegion: ' + JSON.stringify(data.region.identifier + ' : Prox : ' + data.beacons[0].proximity + ' - rssi: ' + data.beacons[0].rssi + ' - tx: ' + data.beacons[0].tx + ' - accuracy: ' + data.beacons[0].accuracy));
-          // var ratio_db = data.beacons[0].tx - data.beacons[0].rssi;
-          // var ratio_linear = Math.pow(10, ratio_db / 10);
-
-          // var r = Math.sqrt(ratio_linear);
-          // console.log(data.region.identifier +'   '+ r);
-          // console.log(data.region.identifier +'   '+ data.beacons[0].rssi);
+          }, function(response) {
+            console.log(response);
+          });
+        } else {
+          console.log("Not In Range");
+        }
       };
     };
     $ionicPlatform.ready(function() {
